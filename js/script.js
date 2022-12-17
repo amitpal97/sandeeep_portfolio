0dae1d1093431b4c30f98fefc44abaa5e4d3e40f
amitpal97 <amitrpal9@gmail.com>
2022-12-18 02:25:34 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("nav_option").click(function(){
+        $("container").addClass(".dsp")
+        // $(".wrapper").show(10000)
+    })
+
+    // $(".nav_option").click(function(){
+    })
+// }
