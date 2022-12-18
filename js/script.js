71b68e5195277f57955598749056ac23ef3310bf
amitpal97 <amitrpal9@gmail.com>
2022-12-18 23:05:49 +0530

    second chaanges

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-$(document).ready(function(){
-    $("nav_option").click(function(){
-        $("container").addClass(".dsp")
-        // $(".wrapper").show(10000)
-    })
 
-    // $(".nav_option").click(function(){
-    })
-// }
+// $(document).ready(function(){
+//     $(".nav_option").click(function(){
+//       $(".container").hide();
+//     });
+//     $(".nav_option").click(function(){
+//       $(".container").show();
+//     });
+// //   });
+// var about_Us=document.querySelector(".nav_option")
+// about_Us.addEventListener
+ function myFunction(){
+// console.log(elm);
+// elm.classList.addClass("dsp")
+var elm =document.getElementsByClassName(".container_About_Us");
+elm.classList.addclass("dsp")
+}
+
+
