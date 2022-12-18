f01d245c941b77dcaa44dbf197009fa309f4de18
amitpal97 <amitrpal9@gmail.com>
2022-12-19 01:23:43 +0530

    all done

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 
-// $(document).ready(function(){
-//     $(".nav_option").click(function(){
-//       $(".container").hide();
-//     });
-//     $(".nav_option").click(function(){
-//       $(".container").show();
-//     });
-// //   });
-// var about_Us=document.querySelector(".nav_option")
-// about_Us.addEventListener
- function myFunction(){
-// console.log(elm);
-// elm.classList.addClass("dsp")
-var elm =document.getElementsByClassName(".container_About_Us");
-elm.classList.addclass("dsp")
-}
 
+$(".nav_option").click(function () {
+  $(document).scrollTop(100); // any value you need
+});
 
+$(".nav_optionAbout").click(function () {
+  $(document).scrollTop(800); // any value you need
+});
+
+$(".nav_optionEducation").click(function () {
+  $(document).scrollTop(1500); // any value you need
+});
+
+$(".nav_optionContac").click(function () {
+  $(document).scrollTop(10000); // any value you need
+});
